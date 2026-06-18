ef6ee71dc3d8f482199815c8a666ea02187029a0
Natalia Baraukova <borovkova2088@gmail.com>
2026-06-19 01:03:33 +0300

    feat: add JwtClaim and ErrorResponse models

---
+++ src/main/java/com/innowise/apigateway/model/JwtClaim.java
@@ -0,0 +1,9 @@
+package com.innowise.apigateway.model;
+/**
+ * Represents extracted claims from a validated JWT token.
+ *
+ * @param userId the ID of the authenticated user
+ * @param role   the role of the authenticated user
+ */
+public record JwtClaim(Long userId, String role) {
+}
