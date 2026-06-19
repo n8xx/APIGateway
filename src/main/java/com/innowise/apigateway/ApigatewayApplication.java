e7e27f5ac2de0ede3ce15240ca5bf1798b7be42b
Natalia Baraukova <borovkova2088@gmail.com>
2026-06-19 03:57:49 +0300

    fix: exclude reactive security auto-configuration

--- src/main/java/com/innowise/apigateway/ApigatewayApplication.java
+++ src/main/java/com/innowise/apigateway/ApigatewayApplication.java
@@ -3,9 +3,14 @@ package com.innowise.apigateway;
 import com.innowise.apigateway.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ReactiveSecurityAutoConfiguration.class,
+        ReactiveUserDetailsServiceAutoConfiguration.class
+})
 @EnableConfigurationProperties(JwtConfig.class)
 public class ApigatewayApplication {
 
