4f133568bd4fa0787ff3284b33b4731be5a45870
Natalia Baraukova <borovkova2088@gmail.com>
2026-06-21 23:33:39 +0300

    style: fix formatting in JwtServiceImpl

--- src/main/java/com/innowise/apigateway/service/impl/JwtServiceImpl.java
+++ src/main/java/com/innowise/apigateway/service/impl/JwtServiceImpl.java
@@ -12,7 +12,6 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
 import javax.crypto.SecretKey;
 
 @Service
@@ -20,9 +19,10 @@ public class JwtServiceImpl implements JwtService {
     private final JwtConfig jwtConfig;
     private SecretKey secretKey;
 
-    public JwtServiceImpl(JwtConfig jwtConfig){
-        this.jwtConfig= jwtConfig;
+    public JwtServiceImpl(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
     }
+
     @PostConstruct
     void init(){
         byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.secret());
